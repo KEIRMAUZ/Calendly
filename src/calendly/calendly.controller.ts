1cc34fee9a8831b5e65dc53da1f7d6efe01ed778
KEIRMAUZ <kevinmanuelarteagaruiz@gmail.com>
2025-06-26 19:10:27 -0500

    [FIX] Reparar calendly service

--- src/calendly/calendly.controller.ts
+++ src/calendly/calendly.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Headers } from '@nestjs/common';
 
 @Controller('calendly')
-export class CalendlyController {}
+export class CalendlyController {
+
+    @Post('/event')
+    handleCalendlyEvent(@Body() payload: any, @Headers() headers: any) {
+    console.log('Evento recibido de Calendly:', payload);
+
+    return { status: 'ok' };
+  }
+}
+
+
+
--- src/calendly/calendly.service.ts
+++ src/calendly/calendly.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
 
 @Injectable()
-export class CalendlyService {}
+export class CalendlyService {
+    constructor(private readonly httpService: HttpService) {}
+
+  async getScheduledEvents() {
+    const token = 'Bearer TU_TOKEN';
+    const url = 'https://api.calendly.com/scheduled_events';
+
+    const response = await this.httpService.axiosRef.get(url, {
+      headers: { Authorization: token },
+    });
+
+    return response.data;
+  }
+}
+
+
+
