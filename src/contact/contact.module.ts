b23c4aa35c890ecb4adbad8b86c86eead9cbf8e0
KEIRMAUZ <kevinmanuelarteagaruiz@gmail.com>
2025-07-17 17:39:25 -0500

    [ADD] Logica de envio de correos

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -3,12 +3,14 @@ import { ContactController } from './contact.controller';
 import { ContactService } from './contact.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Contact, ContactSchema } from './schemas/contact.schema';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Contact.name, schema: ContactSchema }
-    ])
+    ]),
+    ConfigModule,
   ],
   controllers: [ContactController],
   providers: [ContactService],
--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -3,17 +3,46 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Contact, ContactDocument } from './schemas/contact.schema';
 import { CreateContactDto } from './dto/create-contact.dto';
+import { MailtrapClient } from 'mailtrap';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ContactService {
   constructor(
     @InjectModel(Contact.name) private contactModel: Model<ContactDocument>,
+    private readonly configService: ConfigService,
   ) {}
 
   async create(createContactDto: CreateContactDto): Promise<Contact> {
     try {
       const newContact = new this.contactModel(createContactDto);
       const savedContact = await newContact.save();
+
+      // Enviar email con Mailtrap
+      const TOKEN = this.configService.get<string>('API_TOKEN_MAIL');
+      if (!TOKEN) throw new Error('API_TOKEN_MAIL is not set in environment variables');
+      const client = new MailtrapClient({
+        token: TOKEN,
+        // testInboxId: 3901100, // Opcional, si tienes un inbox de test
+      });
+      const sender = {
+        email: 'hello@example.com',
+        name: 'Mailtrap Test',
+      };
+      const recipients = [
+        {
+          email: savedContact.email,
+          name: savedContact.name,
+        },
+      ];
+      await client.send({
+        from: sender,
+        to: recipients,
+        subject: '¡Gracias por tu contacto!',
+        text: `Hola ${savedContact.name}, gracias por contactarnos. Pronto te responderemos.`,
+        category: 'Contacto',
+      });
+
       return savedContact;
     } catch (error) {
       throw new Error(`Error creating contact: ${error.message}`);
