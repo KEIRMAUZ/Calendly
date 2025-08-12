910b5dd25c53304e7f9b29e1bed0cae01e663d6d
KEIRMAUZ <kevinmanuelarteagaruiz@gmail.com>
2025-08-11 19:38:55 -0500

    [ADD] Se añadio el endpoint de versel para cors

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,8 @@ async function bootstrap() {
       'http://127.0.0.1:5174', // Alternativa localhost
       'http://localhost:4173', // Puerto de preview de Vite
       'http://127.0.0.1:4173',
-      'http://localhost:5173/', // Alternativa preview
+      'http://localhost:5173/',
+      'https://viajes-8vgc.vercel.app/', // Alternativa preview
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: [
